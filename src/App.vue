da8a4fbaf36b1122d28811266cd08e813497de88
wahyu1377 <137584974+wahyu1377@users.noreply.github.com>
2025-05-10 21:34:16 +0700

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<!-- App.vue -->
+<template>
+  <div>
+    <h1>Contoh Penggunaan Komponen di Vue </h1>
+
+    <HelloMessage name="Wahyu" @greet="handleGreet">
+
+      <p><em>Ini adalah konten dari parent yang masuk ke slot!</em></p>
+    </HelloMessage>
+  </div>
+</template>
+
+<script setup>
+import HelloMessage from './components/HelloMessage.vue'
+
+function handleGreet(pesan) {
+  alert(pesan)
+}
+</script>
---
+++ src/components/HelloMessage.vue
@@ -0,0 +1,36 @@
+<!-- components/HelloMessage.vue -->
+<template>
+  <div class="box">
+    <h2>Halo, {{ name }}!</h2>
+    <button @click="sendGreeting">Kirim Pesan ke Parent</button>
+    <!-- Slot untuk konten dari parent -->
+    <div class="slot-area">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({ name: String }) // Props
+const emit = defineEmits(['greet'])         // Emits
+
+function sendGreeting() {
+  emit('greet', `Salam dari ${props.name}`) // Emit event
+}
+</script>
+
+<style scoped>
+.box {
+  border: 1px solid #ccc;
+  padding: 1rem;
+  border-radius: 10px;
+}
+.slot-area {
+  margin-top: 1rem;
+  padding: 0.5rem;
+  background: #f9f9f9;
+}
+</style>
+
+
+
